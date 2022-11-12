66158b5bee9e9e3ae7202ce940c7dcbc53f24ef4
Jake Hall <jake.hallslm@gmail.com>
2022-11-12 06:39:43 +0000

    remvoed gallery stuff

--- src/routes/gallery/+page.server.ts
+++ src/routes/gallery/+page.server.ts
@@ -1,26 +1,10 @@
-import fs from 'fs';
 import type { LoadEvent } from '@sveltejs/kit';
-
-let myPromise = new Promise((res, rej) => {
-    fs.readdir('C:/Users/jakeh/Desktop/lilley-and-stone/src/lib/assets/school', async (err, filenames) => {
-        if (err) {
-            rej(err)
-        } else {
-            res(filenames)
-        }
-    })
-})
 
 /** @type {import('./$types').ServerLoad} */
 export async function load({ params }: LoadEvent) {
-    const files = await myPromise.then((value) => {
-        console.log('this is value', value)
-        return value
-    }, (error) => {
-        console.log('error', error)
-    })
+
 
     return {
-        files
+        hello: 'world'
     }
 }
--- src/routes/gallery/+page.svelte
+++ src/routes/gallery/+page.svelte
@@ -1,16 +1,1 @@
-<script>
-	import Gallery from 'svelte-image-gallery';
-
-	import image from '$lib/assets/school/building_1.jpg';
-
-	export let data;
-	console.log(data);
-
-	function handleClick(e) {
-		console.log(e.detail.src);
-	}
-</script>
-
-<Gallery on:click={handleClick}>
-	<img src={image} />
-</Gallery>
+<h1>Gallery</h1>
